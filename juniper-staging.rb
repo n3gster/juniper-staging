2ee60ceb130509b5b30bf1aba5a3aa57af550f6b
Andy Davidson <andy@nosignal.org>
2013-08-31 12:43:30 +0100

    Open a template file and parse it.

--- juniper-staging.rb
+++ juniper-staging.rb
@@ -5,6 +5,6 @@ require 'parseconfig'
 config = ParseConfig.new(ARGV[0])
 template_file = config['template_file']
 unless File.file?(template_file)
-	puts "Check the template_file configuration variable is correct (points to a valid template filename) in #{ARGV[0]}"
+	puts "ERROR: Check the template_file configuration variable is correct (points to a valid template's filename) in #{ARGV[0]}"
 end
 
