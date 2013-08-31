da8971b8e6f12265e49725afdb3d84aa75a1abeb
Andy Davidson <andy@nosignal.org>
2013-08-31 12:44:48 +0100

    Abort the script with an error signal if the template file doesn't seem to exist

--- juniper-staging.rb
+++ juniper-staging.rb
@@ -6,5 +6,6 @@ config = ParseConfig.new(ARGV[0])
 template_file = config['template_file']
 unless File.file?(template_file)
 	puts "ERROR: Check the template_file configuration variable is correct (points to a valid template's filename) in #{ARGV[0]}"
+	abort
 end
 
